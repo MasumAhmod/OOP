9ac4b99202fc247991e9859937044fda5ccb5877
MasumAhmod <masumahmod332@gmail.com>
2025-07-06 11:42:57 +0600

    Update Private Constructor.java

--- OOP/Constructor/Private Constructor.java
+++ OOP/Constructor/Private Constructor.java
@@ -1,1 +1,29 @@
+///Masum
+import java.io.*;
+/*
+Private Constructor:
+Only accessible within the class itself.
+*/
+
+public class Case {
+    String s , p;
+    int n;
+
+    /// Constructor
+    private Case(String s , int n , String p) {
+        this.s = s;
+        this.p = p;
+        this.n = n;
+
+        System.out.println("My Name is " + s + " And I'm " + n + ".");
+        System.out.println("Currently I'm Doing my BSc at " + p);
+    }
+
+    public static void main(String[] args) {
+        Case MyInfo = new Case("Masum" , 22 , "Metropalitan Univarsity");
+    }
+}
+
+
+
 
